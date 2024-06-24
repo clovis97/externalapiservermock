45eeb4b931e87d3daed71720a7450910a6091259
kenmogne tchuinte clovis <jeansam130@gmail.com>
2024-06-24 07:44:45 +0100

    update

--- africellmockapi/views.py
+++ africellmockapi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 class AfricellB2CMockView(APIView):
     def post(self, request, *args, **kwargs):
@@ -307,6 +308,7 @@ class AfricellSendSmsMockView(APIView):
         return Response(response_data)
     
 class AfricellTopUpMockView(APIView):
+    renderer_classes = [JSONRenderer]
     def post(self, request, *args, **kwargs):
         response_data = {
   "Code": '0',
